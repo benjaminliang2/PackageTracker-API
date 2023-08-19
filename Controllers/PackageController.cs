2260d1a793f476a6ec10f0844f4d9c5cf889ee0f
benja <benja@DESKTOP-8T3FLOH>
2023-08-18 14:18:57 -1000

    Add project files.

---
+++ Controllers/PackageController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PackageTracker_API.Controllers
+{
+
+    [Route("api/v0/[controller]")]
+    [ApiController]
+
+    public class PackageController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PackageController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet("{city}")]
+        public async Task<IActionResult> GetWeather(string city)
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+                string apiKEY = "0accd00ee96f87579969974f2b12729d";
+                string apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKEY;
+                var response = await client.GetAsync(apiURL);
+                if (response.IsSuccessStatusCode)
+                {
+                    var weatherData = await response.Content.ReadAsStringAsync();
+                    return Ok(weatherData);
+
+                }
+                else
+                {
+                    return BadRequest("Error fetching weather data");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occured: {ex.Message}");
+
+            }
+        }
+    }
+    public class WeatherResponse
+    {
+        public string Name { get; set; }
+        public MainData Main { get; set; }
+    }
+    public class MainData
+    {
+        public double Temp { get; set; }
+    }
+
+}
